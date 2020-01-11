addb1552628f4c522f0222d90be280adce935b26
heff <ksuder7@gmail.com>
2020-01-11 13:54:00 +0100

    add media to urls

--- shop/urls.py
+++ shop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_page, contact_page, login_page, register_page
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", home_page),
@@ -23,4 +25,4 @@ urlpatterns = [
     path("login/", login_page),
     path("register/", register_page),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
