96b28187560740c75ce476f6e6cd8b39eea9de2c
Krzysztof Suder <ksuder7@gmail.com>
2020-01-23 22:00:26 +0100

    add product list and detail view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
